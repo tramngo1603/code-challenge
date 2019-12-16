ebae45aa58d2cc7d41993586dd168d46d0eadc41
Tram Ngo <tramngo1603@gmail.com>
2019-12-15 21:05:53 -0800

    Add pytest test

---
+++ test_greater.py
@@ -0,0 +1,13 @@
+from immediate_greatest import findGreater
+
+def test_myfunc1():
+    result1 = findGreater(12345967)
+    assert result1 == 12345976
+
+def test_myfunc2():
+    result1 = findGreater(54321)
+    assert result1 == 54321
+
+def test_myfunc3():
+    result1 = findGreater(78960231)
+    assert result1 == 78960312
